d7e397fa0b9d83fef1f82acdd4035fb93cc78d06
Barabás Balázs <barabas.balazska@gmail.com>
2022-11-05 17:53:26 +0200

    migrated db to cluster

--- backend/services/db-service.js
+++ backend/services/db-service.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
+import constants from '../utils/constants.js';
+
+const { passwordToCluster } = constants;
 
 const connectToDb = () => {
-  mongoose.connect('mongodb://localhost:27017/trains', { useNewUrlParser: true }, (err) => {
+  mongoose.connect(`mongodb+srv://Vargabeela:${passwordToCluster}@cluster0.osgnhmn.mongodb.net/trains`, { useNewUrlParser: true }, (err) => {
     if (err) {
       throw err;
     }
--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,8 +1,7 @@
-const secret = '4234hdh9ghf38rdnfocnw4';
-
 const constants = {
   frontendOrigin: 'http://localhost:3000',
   secret: '4234hdh9ghf38rdnfocnw4',
+  passwordToCluster: '',
 };
 
 export default constants;
